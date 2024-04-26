dd6f7ee9a3d1f2ce06905c13def73be04a7a817f
Surdyanu <maryna.va.vasyl@student.utcluj.ro>
2024-04-26 23:06:15 +0300

    Initial Commit

---
+++ cashdine1/cashdine1App.swift
@@ -0,0 +1,17 @@
+//
+//  cashdine1App.swift
+//  cashdine1
+//
+//  Created by Vasyl Maryna on 26.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct cashdine1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
